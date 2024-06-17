bcdd64ba3c8971f1afc80843fb7e8716440999fe
jay-parkin <jay-parkin91@hotmail.com>
2024-06-17 22:37:56 +1000

    Added login_user()

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
+from flask_jwt_extended import create_access_token
+
 from models.user import User, user_schema
 from init import bcrypt, db
 
@@ -42,3 +46,25 @@ def register_user():
 
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use"}, 409
+        
+@auth_bp.route("/login", methods=["POST"])
+def login_user():
+    # get data from the body of the request
+    body_data = request.get_json()
+    
+    # find the user with the email
+    stmt = db.select(User).filter_by(email = body_data.get("email"))
+    user = db.session.scalar(stmt)
+    
+    # if user exists? and password is correct?
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # create jwt
+        token = create_access_token(identity = str(user.id), expires_delta = timedelta(days = 1))
+
+        # respond back
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+
+    # else
+    else:
+        # respond back with erorr message
+        return {"error": "Invalid email or password"}, 401 # unauthenicated
