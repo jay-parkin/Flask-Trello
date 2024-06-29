29a6a360746a0ee759796f58a4341a4f9fb86b16
jay-parkin <jay-parkin91@hotmail.com>
2024-06-29 12:52:39 +1000

    Authorise utils created

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -6,6 +6,8 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from init import db
 from models.card import Card, card_schema, cards_schema
 from controllers.comment_controller import comments_bp
+from models.user import User
+from utils import authorise_as_admin
 
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
@@ -64,6 +66,11 @@ def create_card():
 @cards_bp.route("/<int:card_id>", methods=["DELETE"])
 @jwt_required()
 def delete_card(card_id):
+    # check whether the user is an admin or not
+    is_admin = authorise_as_admin()
+    if not is_admin:
+        return {"error": "User is not authorised to perform this action."}, 403
+
     # fetch card from database
     stmt = db.select(Card).filter_by(id = card_id)
     card = db.session.scalar(stmt)
@@ -91,6 +98,10 @@ def update_card(card_id):
 
     # if card exist
     if card:
+        # if the user is the owner
+        if str(card.user_id) != get_jwt_identity():
+            return {"error": "You are not the owner of the card"}, 403
+        
         # update the fields
         card.title = body_data.get("title") or card.title,
         card.description = body_data.get("description") or card.description,
